c46c70b93f56abf4d068cd73098d8165d7ab4607
Nicolas <nicolas.moyon@ynov.com>
2025-02-18 09:55:43 +0100

    udpate

--- migrations/000001_create_users.up.sql
+++ migrations/000001_create_users.up.sql
@@ -1,13 +1,16 @@
- CREATE TABLE IF NOT EXISTS users (
-     id         bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY
-     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-     username       TEXT NOT NULL,
-     email      CITEXT UNIQUE NOT NULL,
-     address    TEXT,
-     password   Bytea NOT NULL,
-     user_role   TEXT,
-     status     TEXT,
-     shop       TEXT,
-     version    INT DEFAULT 1
-     );
+-- Enable citext extension if not already enabled
+CREATE EXTENSION IF NOT EXISTS citext;
+
+CREATE TABLE IF NOT EXISTS users (
+                                     id          BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+                                     created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                     updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                     username    TEXT NOT NULL,
+                                     email       CITEXT UNIQUE NOT NULL,
+                                     address     TEXT,
+                                     password    BYTEA NOT NULL,
+                                     user_role   TEXT,
+    status      TEXT,
+    shop        TEXT,
+    version     INT DEFAULT 1
+    );
