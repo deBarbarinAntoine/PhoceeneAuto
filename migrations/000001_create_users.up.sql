48a4032e7a0080a44a599fc37be972bdae4a5838
Nicolas <nicolas.moyon@ynov.com>
2025-02-18 09:23:00 +0100

    correction migrations

--- migrations/000001_create_users.up.sql
+++ migrations/000001_create_users.up.sql
@@ -1,10 +1,10 @@
  CREATE TABLE IF NOT EXISTS users (
      id         INT PRIMARY KEY AUTO_INCREMENT,
      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
      username       TEXT NOT NULL,
-     email      TEXT UNIQUE NOT NULL,
-     address    CITEXT,
+     email      CITEXT UNIQUE NOT NULL,
+     address    TEXT,
      password   Bytea NOT NULL,
      userRole   TEXT,
      status     TEXT,
--- migrations/000004_create_car_products.up.sql
+++ migrations/000004_create_car_products.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS car_products (
     id          INT PRIMARY KEY AUTO_INCREMENT,
     created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     status      TEXT,
     kilometers  FLOAT,
     owner_nb    INT,
@@ -10,5 +10,5 @@ CREATE TABLE IF NOT EXISTS car_products (
     shop        TEXT,
     version     INT DEFAULT 1,
     cat_id INT,
-    FOREIGN KEY (cat_id) REFERENCES cars_catalog(id) ON DELETE SET NULL
+    FOREIGN KEY (cat_id) REFERENCES cars_catalog(id)
     );
