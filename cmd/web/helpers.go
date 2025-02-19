b7c22b4a779be7645a83eb39195b7595bb6fa648
Esabrina77 <sabrina.eloundou@ynov.com>
2025-02-19 09:16:30 +0100

    pages

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -12,10 +12,10 @@ import (
 	"runtime/debug"
 	"strconv"
 	"time"
-	
+
 	"PhoceeneAuto/internal/data"
 	"PhoceeneAuto/internal/validator"
-	
+
 	"github.com/alexedwards/flow"
 	"github.com/go-playground/form/v4"
 	"github.com/justinas/nosurf"
@@ -54,7 +54,7 @@ func (app *application) cleanExpiredDeletedClients(frequency, timeout time.Durat
 }
 
 func (app *application) logout(r *http.Request) error {
-	
+
 	err := app.sessionManager.Clear(r.Context())
 	if err != nil {
 		return err
@@ -63,7 +63,7 @@ func (app *application) logout(r *http.Request) error {
 	if err != nil {
 		return err
 	}
-	
+
 	return nil
 }
 
@@ -86,55 +86,55 @@ func (app *application) getNonce(r *http.Request) string {
 }
 
 func (app *application) decodePostForm(r *http.Request, dst any) error {
-	
+
 	err := r.ParseForm()
 	if err != nil {
 		return err
 	}
-	
+
 	err = app.formDecoder.Decode(dst, r.PostForm)
 	if err != nil {
 		var invalidDecoderError *form.InvalidDecoderError
-		
+
 		if errors.As(err, &invalidDecoderError) {
 			panic(err)
 		}
-		
+
 		return err
 	}
-	
+
 	return nil
 }
 
 func (app *application) clientError(w http.ResponseWriter, r *http.Request, status int) {
-	
+
 	// setting the templateData
 	tmplData := app.newTemplateData(r)
-	
+
 	// setting the error title and message
 	tmplData.Error.Title = fmt.Sprintf("Error %d", status)
-	
+
 	if status == http.StatusNotFound {
 		tmplData.Error.Message = "We didn't find what you were looking for :("
 	} else {
 		tmplData.Error.Message = "Something went wrong!"
 	}
-	
+
 	// rendering the error page
 	app.render(w, r, status, "error.tmpl", tmplData)
 }
 
 func (app *application) failedValidationError(w http.ResponseWriter, r *http.Request, form any, v *validator.Validator, page string) {
-	
+
 	// DEBUG
 	app.logger.Debug(fmt.Sprintf("generic errors: %+v", v.NonFieldErrors))
 	app.logger.Debug(fmt.Sprintf("field errors: %+v", v.FieldErrors))
-	
+
 	// retrieving basic template data
 	tmplData := app.newTemplateData(r)
-	
+
 	tmplData.Form = form
-	
+
 	// render the template
 	app.render(w, r, http.StatusUnprocessableEntity, page, tmplData)
 }
@@ -146,53 +146,53 @@ func (app *application) serverError(w http.ResponseWriter, r *http.Request, err 
 		uri    = r.URL.RequestURI()
 		trace  = string(debug.Stack())
 	)
-	
+
 	// logging the error
 	app.logger.Error(err.Error(), slog.String("method", method), slog.String("URI", uri), slog.String("trace", trace))
-	
+
 	// setting the templateData
 	tmplData := app.newTemplateData(r)
-	
+
 	// setting the error title and message
 	tmplData.Error.Title = fmt.Sprintf("Error %d", status)
 	tmplData.Error.Message = "Something went wrong!"
-	
+
 	// rendering the error page
 	app.render(w, r, status, "error.tmpl", tmplData)
 }
 
 func (app *application) ajaxResponse(w http.ResponseWriter, status int, msg string) {
-	
+
 	// setting the response data
 	var resData envelope
-	
+
 	// checking the status code
 	if status < http.StatusBadRequest {
-		
+
 		// wrapping the message in a JSON object
 		resData = envelope{"response": msg}
-		
+
 	} else {
 		// logging the error
 		app.logger.Error(msg)
-		
+
 		// wrapping error in JSON object
 		resData = envelope{"error": "internal server error"}
 	}
-	
+
 	// marshalling the resData
 	jsonData, err := json.Marshal(resData)
 	if err != nil {
 		app.logger.Error(err.Error())
 		return
 	}
-	
+
 	// setting the Content-Type header to JSON
 	w.Header().Set("Content-Type", "application/jsonData")
-	
+
 	// setting the Status response
 	w.WriteHeader(status)
-	
+
 	// send the response with the JSON data
 	_, err = w.Write(jsonData)
 	if err != nil {
@@ -201,20 +201,20 @@ func (app *application) ajaxResponse(w http.ResponseWriter, status int, msg stri
 }
 
 func (app *application) background(fn func()) {
-	
+
 	app.wg.Add(1)
 	go func() {
-		
+
 		defer app.wg.Done()
-		
+
 		defer func() {
 			if err := recover(); err != nil {
 				app.logger.Error(fmt.Sprintf("%v", err))
 			}
 		}()
-		
+
 		fn()
-		
+
 	}()
 }
 
@@ -223,7 +223,7 @@ func (app *application) isAuthenticated(r *http.Request) bool {
 	if !ok {
 		return false
 	}
-	
+
 	return isAuthenticated
 }
 
@@ -248,30 +248,30 @@ func newUserLoginForm() *userLoginForm {
 }
 
 func newUserUpdateForm(user *data.User) *userUpdateForm {
-	
+
 	// creating the form
 	var formUpdateUser = new(userUpdateForm)
-	
+
 	// filling the form with the data if any
 	if user != nil {
 		formUpdateUser.Username = &user.Name
 		formUpdateUser.Email = &user.Email
 	}
-	
+
 	// setting the validator
 	formUpdateUser.Validator = *validator.New()
-	
+
 	return formUpdateUser
 }
 
 func (app *application) newTemplateData(r *http.Request) templateData {
-	
+
 	// checking is the user is authenticated
 	isAuthenticated := app.isAuthenticated(r)
-	
+
 	// retrieving the nonce
 	nonce := app.getNonce(r)
-	
+
 	// returning the templateData with all information
 	var tmplData = templateData{
 		CurrentYear:     time.Now().Year(),
@@ -287,22 +287,22 @@ func (app *application) newTemplateData(r *http.Request) templateData {
 			Message: "We didn't find what you were looking for :(",
 		},
 	}
-	
+
 	return tmplData
 }
 
 func (app *application) render(w http.ResponseWriter, r *http.Request, status int, page string, data templateData) {
-	
+
 	// retrieving the appropriate set of templates
 	ts, ok := app.templateCache[page]
 	if !ok {
 		app.serverError(w, r, fmt.Errorf("the template %s does not exist", page))
 		return
 	}
-	
+
 	// creating a bytes Buffer
 	buf := new(bytes.Buffer)
-	
+
 	// executing the template in the buffer to catch any possible parsing error,
 	// so that the user doesn't see a half-empty page
 	err := ts.ExecuteTemplate(buf, "base", data)
@@ -310,29 +310,29 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, status in
 		app.serverError(w, r, err)
 		return
 	}
-	
+
 	// if it's all okay, write the status in the header and write the buffer in the ResponseWriter
 	w.WriteHeader(status)
-	
+
 	buf.WriteTo(w)
 }
 
 func getPathID(r *http.Request) (int, error) {
-	
+
 	// fetching the id param from the URL
 	param := flow.Param(r.Context(), "id")
-	
+
 	// looking for errors
 	if param == "" {
 		return 0, fmt.Errorf("id param required")
 	}
-	
+
 	// converting the param to int
 	id, err := strconv.Atoi(param)
 	if err != nil || id < 1 {
 		return 0, fmt.Errorf("invalid id param: %w", err)
 	}
-	
+
 	// return the integer id
 	return id, nil
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,10 +8,10 @@ import (
 	"os"
 	"sync"
 	"time"
-	
+
 	"PhoceeneAuto/internal/data"
 	"PhoceeneAuto/internal/mailer"
-	
+
 	"github.com/alexedwards/scs/postgresstore"
 	"github.com/alexedwards/scs/v2"
 	"github.com/go-playground/form/v4"
@@ -19,55 +19,55 @@ import (
 )
 
 func main() {
-	
+
 	// setting the configuration variables
 	var cfg config
-	
+
 	// generic variables
 	flag.Int64Var(&cfg.port, "port", 8080, "HTTP service address")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
-	
+
 	// PostgreSQL variables
 	flag.StringVar(&cfg.db.dsn, "dsn", "", "PostgreSQL Database DSN")
-	
+
 	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
 	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
 	flag.DurationVar(&cfg.db.maxIdleTime, "db-max-idle-time", 15*time.Minute, "PostgreSQL max connection idle time")
-	
+
 	// SMTP variables
 	flag.StringVar(&cfg.smtp.host, "smtp-host", "", "SMTP host")
 	flag.Int64Var(&cfg.smtp.port, "smtp-port", 2525, "SMTP port")
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Phoceene Auto <no-reply@phoceene-auto.com", "SMTP sender")
-	
+
 	// cleaning frequency
 	frequency := flag.Duration("frequency", time.Hour*2, "expired tokens and unactivated users cleaning frequency")
-	
+
 	flag.Parse()
-	
+
 	// setting the logging level according to the environment
 	var opts *slog.HandlerOptions
-	
+
 	if cfg.env == "development" {
 		opts = &slog.HandlerOptions{Level: slog.LevelDebug}
 	} else {
 		opts = &slog.HandlerOptions{Level: slog.LevelInfo}
 	}
 	logger := slog.New(slog.NewTextHandler(os.Stdout, opts))
-	
+
 	// checking the SMTP info
 	if cfg.smtp.username == "" || cfg.smtp.password == "" || cfg.smtp.host == "" {
 		fmt.Println("SMTP credentials are required")
 		os.Exit(1)
 	}
-	
+
 	// checking the dsn info
 	if cfg.db.dsn == "" {
 		logger.Error("dsn is required")
 		os.Exit(1)
 	}
-	
+
 	// connecting to the database
 	db, err := openDB(cfg.db.dsn)
 	if err != nil {
@@ -76,22 +76,22 @@ func main() {
 	}
 	defer db.Close()
 	logger.Info("DB pool connection opened successfully!")
-	
+
 	// caching the templates
 	templateCache, err := newTemplateCache()
 	if err != nil {
 		logger.Error(err.Error())
 		os.Exit(1)
 	}
-	
+
 	// initializing the application components
 	formDecoder := form.NewDecoder()
-	
+
 	sessionManager := scs.New()
 	sessionManager.Store = postgresstore.New(db)
 	sessionManager.Lifetime = 24 * time.Hour
 	sessionManager.Cookie.Secure = true
-	
+
 	app := &application{
 		logger:         logger,
 		mailer:         mailer.New(cfg.smtp.host, int(cfg.smtp.port), cfg.smtp.username, cfg.smtp.password, cfg.smtp.sender),
@@ -102,35 +102,35 @@ func main() {
 		models:         data.NewModels(db),
 		wg:             new(sync.WaitGroup),
 	}
-	
+
 	// Clean expired tokens every N duration with no timeout
 	go app.cleanExpiredTokens(*frequency, time.Hour*0)
-	
+
 	// Clean deleted clients after a legal period (RGPD) after an hour timeout
 	go app.cleanExpiredDeletedClients(*frequency, time.Hour*1)
-	
+
 	// Running the server
 	err = app.serve()
 	if err != nil {
 		logger.Error(err.Error())
 		os.Exit(1)
 	}
-	
+
 	os.Exit(1)
 }
 
 func openDB(dsn string) (*sql.DB, error) {
-	
+
 	db, err := sql.Open("postgres", dsn)
 	if err != nil {
 		return nil, err
 	}
-	
+
 	err = db.Ping()
 	if err != nil {
 		db.Close()
 		return nil, err
 	}
-	
+
 	return db, nil
 }
