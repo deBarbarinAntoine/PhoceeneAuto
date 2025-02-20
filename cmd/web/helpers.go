bb31f3836daeb09acad60ac59d77958f5c625334
Thorgan <amdebarbarin@gmail.com>
2025-02-20 22:59:23 +0100

    Refactor templates & Add cars.json for the database.

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -12,10 +12,10 @@ import (
 	"runtime/debug"
 	"strconv"
 	"time"
-	
+
 	"PhoceeneAuto/internal/data"
 	"PhoceeneAuto/internal/validator"
-	
+
 	"github.com/alexedwards/flow"
 	"github.com/go-playground/form/v4"
 	"github.com/justinas/nosurf"
@@ -53,7 +53,7 @@ func (app *application) cleanExpiredDeletedClients(frequency, timeout time.Durat
 //
 //	error - If any error occurs during the process
 func (app *application) logout(r *http.Request) error {
-	
+
 	err := app.sessionManager.Clear(r.Context())
 	if err != nil {
 		return err
@@ -62,7 +62,7 @@ func (app *application) logout(r *http.Request) error {
 	if err != nil {
 		return err
 	}
-	
+
 	return nil
 }
 
@@ -110,23 +110,23 @@ func (app *application) getNonce(r *http.Request) string {
 //
 //	error - If any error occurs during the decoding process
 func (app *application) decodePostForm(r *http.Request, dst any) error {
-	
+
 	err := r.ParseForm()
 	if err != nil {
 		return err
 	}
-	
+
 	err = app.formDecoder.Decode(dst, r.PostForm)
 	if err != nil {
 		var invalidDecoderError *form.InvalidDecoderError
-		
+
 		if errors.As(err, &invalidDecoderError) {
 			panic(err)
 		}
-		
+
 		return err
 	}
-	
+
 	return nil
 }
 
@@ -138,19 +138,19 @@ func (app *application) decodePostForm(r *http.Request, dst any) error {
 //	r - The HTTP request
 //	status - The HTTP status code
 func (app *application) clientError(w http.ResponseWriter, r *http.Request, status int) {
-	
+
 	// setting the templateData
 	tmplData := app.newTemplateData(r)
-	
+
 	// setting the error title and message
 	tmplData.Error.Title = fmt.Sprintf("Error %d", status)
-	
+
 	if status == http.StatusNotFound {
 		tmplData.Error.Message = "We didn't find what you were looking for :("
 	} else {
 		tmplData.Error.Message = "Something went wrong!"
 	}
-	
+
 	// rendering the error page
 	app.render(w, r, status, "error.tmpl", tmplData)
 }
@@ -165,16 +165,16 @@ func (app *application) clientError(w http.ResponseWriter, r *http.Request, stat
 //	v - The validator instance
 //	page - The template page to render
 func (app *application) failedValidationError(w http.ResponseWriter, r *http.Request, form any, v *validator.Validator, page string) {
-	
+
 	// DEBUG
 	app.logger.Debug(fmt.Sprintf("generic errors: %+v", v.NonFieldErrors))
 	app.logger.Debug(fmt.Sprintf("field errors: %+v", v.FieldErrors))
-	
+
 	// retrieving basic template data
 	tmplData := app.newTemplateData(r)
-	
+
 	tmplData.Form = form
-	
+
 	// render the template
 	app.render(w, r, http.StatusUnprocessableEntity, page, tmplData)
 }
@@ -193,17 +193,17 @@ func (app *application) serverError(w http.ResponseWriter, r *http.Request, err 
 		uri    = r.URL.RequestURI()
 		trace  = string(debug.Stack())
 	)
-	
+
 	// logging the error
 	app.logger.Error(err.Error(), slog.String("method", method), slog.String("URI", uri), slog.String("trace", trace))
-	
+
 	// setting the templateData
 	tmplData := app.newTemplateData(r)
-	
+
 	// setting the error title and message
 	tmplData.Error.Title = fmt.Sprintf("Error %d", status)
 	tmplData.Error.Message = "Something went wrong!"
-	
+
 	// rendering the error page
 	app.render(w, r, status, "error.tmpl", tmplData)
 }
@@ -216,37 +216,37 @@ func (app *application) serverError(w http.ResponseWriter, r *http.Request, err 
 //	status - The HTTP status code
 //	msg - The message to send in the response
 func (app *application) ajaxResponse(w http.ResponseWriter, status int, msg string) {
-	
+
 	// setting the response data
 	var resData envelope
-	
+
 	// checking the status code
 	if status < http.StatusBadRequest {
-		
+
 		// wrapping the message in a JSON object
 		resData = envelope{"response": msg}
-		
+
 	} else {
 		// logging the error
 		app.logger.Error(msg)
-		
+
 		// wrapping error in JSON object
 		resData = envelope{"error": "internal server error"}
 	}
-	
+
 	// marshalling the resData
 	jsonData, err := json.Marshal(resData)
 	if err != nil {
 		app.logger.Error(err.Error())
 		return
 	}
-	
+
 	// setting the Content-Type header to JSON
 	w.Header().Set("Content-Type", "application/jsonData")
-	
+
 	// setting the Status response
 	w.WriteHeader(status)
-	
+
 	// send the response with the JSON data
 	_, err = w.Write(jsonData)
 	if err != nil {
@@ -260,20 +260,20 @@ func (app *application) ajaxResponse(w http.ResponseWriter, status int, msg stri
 //
 //	fn - The function to run
 func (app *application) background(fn func()) {
-	
+
 	app.wg.Add(1)
 	go func() {
-		
+
 		defer app.wg.Done()
-		
+
 		defer func() {
 			if err := recover(); err != nil {
 				app.logger.Error(fmt.Sprintf("%v", err))
 			}
 		}()
-		
+
 		fn()
-		
+
 	}()
 }
 
@@ -291,7 +291,7 @@ func (app *application) isAuthenticated(r *http.Request) bool {
 	if !ok {
 		return false
 	}
-	
+
 	return isAuthenticated
 }
 
@@ -339,33 +339,33 @@ func (app *application) getUserRole(r *http.Request) string {
 //
 //	templateData - The template data containing various information
 func (app *application) newTemplateData(r *http.Request) templateData {
-	
+
 	// checking is the user is authenticated
 	isAuthenticated := app.isAuthenticated(r)
-	
+
 	// retrieving the nonce
 	nonce := app.getNonce(r)
-	
+
 	// setting the user variable
 	user := data.AnonymousUser
-	
+
 	// getting the user if authenticated
 	if isAuthenticated {
 		var err error
 		user, err = app.models.UserModel.GetByID(app.getUserID(r))
 		if err != nil {
-			
+
 			// LOGGING
 			app.logger.Warn(fmt.Errorf("user not found: %s", user.ID).Error())
 			user = data.AnonymousUser
-			
+
 		} else {
-			
+
 			// update user role in the SessionManager
 			app.sessionManager.Put(r.Context(), userRoleSessionManager, user.Role)
 		}
 	}
-	
+
 	// returning the templateData with all information
 	var tmplData = templateData{
 		CurrentYear:     time.Now().Year(),
@@ -382,7 +382,7 @@ func (app *application) newTemplateData(r *http.Request) templateData {
 			Message: "We didn't find what you were looking for :(",
 		},
 	}
-	
+
 	return tmplData
 }
 
@@ -396,17 +396,17 @@ func (app *application) newTemplateData(r *http.Request) templateData {
 //	page - The template page to render
 //	data - The template data
 func (app *application) render(w http.ResponseWriter, r *http.Request, status int, page string, data templateData) {
-	
+
 	// retrieving the appropriate set of templates
 	ts, ok := app.templateCache[page]
 	if !ok {
 		app.serverError(w, r, fmt.Errorf("the template %s does not exist", page))
 		return
 	}
-	
+
 	// creating a bytes Buffer
 	buf := new(bytes.Buffer)
-	
+
 	// executing the template in the buffer to catch any possible parsing error,
 	// so that the user doesn't see a half-empty page
 	err := ts.ExecuteTemplate(buf, "base", data)
@@ -414,10 +414,10 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, status in
 		app.serverError(w, r, err)
 		return
 	}
-	
+
 	// if it's all okay, write the status in the header and write the buffer in the ResponseWriter
 	w.WriteHeader(status)
-	
+
 	buf.WriteTo(w)
 }
 
@@ -432,21 +432,21 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, status in
 //	int - The integer ID, or 0 if not found
 //	error - If any error occurs during the process
 func getPathID(r *http.Request) (int, error) {
-	
+
 	// fetching the id param from the URL
 	param := flow.Param(r.Context(), "id")
-	
+
 	// looking for errors
 	if param == "" {
 		return 0, fmt.Errorf("id param required")
 	}
-	
+
 	// converting the param to int
 	id, err := strconv.Atoi(param)
 	if err != nil || id < 1 {
 		return 0, fmt.Errorf("invalid id param: %w", err)
 	}
-	
+
 	// return the integer id
 	return id, nil
 }
