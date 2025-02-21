fd90e45816bf94653966b8895f9b3a68fcf5e683
Thorgan <amdebarbarin@gmail.com>
2025-02-21 09:24:45 +0100

    Fix data insertions in migrations.

--- migrations/000011_transactions_insert.up.sql
+++ migrations/000011_transactions_insert.up.sql
@@ -5,6 +5,6 @@ INSERT INTO transactions (
     status,
     lease_amount
 ) VALUES
-(2, 1, 'PROCESSING', ARRAY[2500.00]),
-(3, 2, 'ONGOING', ARRAY[1500.00, 1500.00]),
-(4, 3, 'DONE', ARRAY[]::FLOAT[]);
+(1, 1, 'PROCESSING', ARRAY[2500.00]),
+(2, 2, 'ONGOING', ARRAY[1500.00, 1500.00]),
+(3, 3, 'DONE', ARRAY[]::FLOAT[]);
